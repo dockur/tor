6982ae0fd74964c52d8f74100d6c9d2df46fc323
Kroese <kroese@users.noreply.github.com>
2026-05-18 03:37:02 +0200

    Make healthcheck optional

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,5 +1,21 @@
-#!/bin/sh
-set -e
+!/usr/bin/env bash
+set -Eeuo pipefail
 
-exec /usr/local/bin/healthcheck
+: "${CHECK:="N"}"
+
+if [[ "$CHECK" != [Yy1]* ]]; then
+  echo "Healthcheck disabled, set the CHECK=Y variable to enable."
+  exit 0
+fi
+
+{ /usr/local/bin/healthcheck; rc=$?; } || :
+(( rc != 0 )) && exit $rc
+
+resp=$(curl -x socks5h://127.0.0.1:9050 'https://check.torproject.org/api/ip')
+
+if ! grep -qm1 -E '"IsTor"\s*:\s*true' <<< "$resp"; then
+  echo "$resp" && exit 1
+fi
+
+echo "Healthcheck OK"
 exit 0
